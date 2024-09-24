457273c4a643e140c238141dedc6c1ac0c018bf9
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-09-24 16:17:26 -0300

    feat: add schema for register and adjustment on login schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,13 +1,25 @@
-import * as z from "zod";
+import { object, string } from "zod";
 
-export const loginSchema = z.object({
-  email: z
-    .string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email"),
-  password: z
-    .string({ required_error: "Password is required" })
-    .min(1, "Password is required")
-    .min(6, "Password must be more than 6 characters")
-    .max(32, "Password must be less than 32 characters"),
+export const loginSchema = object({
+  email: string({ required_error: "O e-mail é obrigatório" })
+    .min(1, "O e-mail é obrigatório")
+    .email("E-mail inválido"),
+  password: string({ required_error: "A senha é obrigatória" }).min(
+    1,
+    "A senha é obrigatória"
+  ),
+});
+
+export const registerSchema = object({
+  email: string({ required_error: "O e-mail é obrigatório" })
+    .min(1, "O e-mail é obrigatório")
+    .email("E-mail inválido"),
+  password: string({ required_error: "A senha é obrigatória" })
+    .min(1, "A senha é obrigatória")
+    .min(6, "A senha deve ter mais de 6 caracteres")
+    .max(32, "A senha deve ter menos de 32 caracteres"),
+  name: string({ required_error: "O nome é obrigatório" }).min(
+    1,
+    "O nome é obrigatório"
+  ),
 });
