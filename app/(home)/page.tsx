6cd1d04bd2940ba1480a2daf2a6b2cc1f98f3a8b
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-09-24 15:16:24 -0300

    feat: add button login on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "../auth/login/_components/login-button";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col h-full items-center justify-center bg-gradient-to-r from-blue-500 to-teal-400">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">Um serviço simples de autenticação</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
     </div>
   );
 }
