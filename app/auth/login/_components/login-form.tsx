db85d60da6fb6cc551666bba8fd36d98478dfb8b
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-09-24 15:12:12 -0300

    feat: add components the auth pages

---
+++ app/auth/login/_components/login-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { loginSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <CardWrapper
+      headerLabel="Bem vindo de volta"
+      backButtonLabel="Não tem uma conta?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="name@exemplo.com"
+                      {...field}
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input placeholder="******" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message="" />
+          <FormSuccess message="" />
+          <Button type="submit" className="w-full">
+            Entrar
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
---
+++ components/auth/social.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
+export const Social = () => {
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FcGoogle className="w-5 h-5" />
+      </Button>
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FaGithub className="w-5 h-5" />
+      </Button>
+    </div>
+  );
+};
