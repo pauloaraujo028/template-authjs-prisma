c294a6c7e8692136f4088abf9fba13fff057ddfc
Paulo Araújo <pauloaraujo.phz@gmail.com>
2024-09-24 12:05:32 -0300

    feat: add the initial authjs config

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+export const { auth: middleware } = NextAuth(authConfig);
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
